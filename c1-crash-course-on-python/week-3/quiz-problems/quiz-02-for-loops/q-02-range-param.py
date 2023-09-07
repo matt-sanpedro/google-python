f3e0923721e5984e4e64492c8541bdfacbb2fe84
matt-sanpedro <m.sanpedro@tcs.com>
2023-09-07 16:17:58 -0700

    Added quiz problems for for loops

---
+++ c1-crash-course-on-python/week-3/quiz-problems/quiz-02-for-loops/q-02-range-param.py
@@ -0,0 +1,3 @@
+# fix this loop to print numbers 12, 18, 24, 30, 36
+for n in range(6,19,3):
+    print(n*2)
---
+++ c1-crash-course-on-python/week-3/quiz-problems/quiz-02-for-loops/q-03-print-even.py
@@ -0,0 +1,12 @@
+for n in range(19):
+    if n % 2 == 0:
+        print(n)
+
+for n in range(18+1):
+    print(n**2)
+
+for n in range(0,18+1,2):
+    print(n*2)
+
+for n in range(10):
+    print(n+n)
