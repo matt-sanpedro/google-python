ae8c1145bbc3cf58ed18b6ab27429994c26d8de0
matt-sanpedro <m.sanpedro@tcs.com>
2023-09-25 11:30:09 -0700

    Added notes and coding examples on docstrings and completed Jupyter notebook code exercise on elevators

--- c1-crash-course-on-python/week-5/e-02-methods-classes.py
+++ c1-crash-course-on-python/week-5/e-02-methods-classes.py
@@ -26,14 +26,56 @@ print(piggy.pig_years())
 
 # __init__ special method
 class Apple:
+    """Outputs the apple color and flavor defined when instantiated"""
     def __init__(self, color, flavor):
         self.color = color
         self.flavor = flavor
 
     # __str__ special method that will display the string when print function is called
+    # GOOD practice: how class used to define a __str__method
+    """Prints the attributes of the Apple class"""
     def __str__(self):
         return "This apple is {} and its flavor is {}".format(self.color, self.flavor)
 
 golden_apple = Apple('yellow', 'soft')
 print(golden_apple.color, golden_apple.flavor)
 print(golden_apple)
+print(help(Apple))
+
+def to_seconds(hours, minutes, seconds):
+    """Returns the amount of seconds in the given hours, minutes, and seconds"""
+    return hours*3600 + minutes*60 + seconds
+help(to_seconds)
+
+# Jupyter notebook code, elevator exercise
+class Elevator:
+    def __init__(self, bottom, top, current):
+        """Initializes the Elevator instance."""
+        self.bottom = bottom
+        self.top = top
+        self.current = current
+    def up(self):
+        """Makes the elevator go up one floor."""
+        self.current += 1
+        if self.current > self.top:
+           self.current = self.top
+        return self.current
+    def down(self):
+        """Makes the elevator go down one floor."""
+        # cannot go below floor 1
+        self.current -= 1
+        if self.current < self.bottom:
+           self.current = self.bottom
+        return self.current
+    def go_to(self, floor):
+        """Makes the elevator go to the specific floor."""
+        self.current = floor
+        if self.current > self.top:
+           self.current = self.top
+        elif self.current < self.bottom:
+           self.current = self.bottom
+        return self.current
+    def __str__(self):
+        return "Current floor: {}".format(self.current)
+
+elevator = Elevator(-1, 10, 0)
--- c1-crash-course-on-python/week-5/p-02-methods-classes.md
+++ c1-crash-course-on-python/week-5/p-02-methods-classes.md
@@ -4,3 +4,16 @@
 - __Methods__: functions that operate on the attributes of a specific instance of a class
 - __Instance variables__: variables that have different values for different instances of the same class
 - __Self__: represents the instance the method is being executed on
+- __Docstring__: a brief text that explains what something does
+
+### Concept: Classes and Instances
+- Classes define the behavior of all instances of a specific class.
+- Each variable of a specific class is an instance or object.
+- Objects can have attributes, which store information about the object.
+- You can make objects do work by calling their methods.
+- The first parameter of the methods (self) represents the current instance.
+- Methods are just like functions, but they can only be used through a class.
+
+### Concept: Special Methods
+- Special methods start and end with __.
+- Special methods have specific names, like __init__ for the constructor or __str__ for the conversion to string.
