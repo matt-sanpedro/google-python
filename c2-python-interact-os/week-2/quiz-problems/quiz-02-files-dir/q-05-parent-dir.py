ec23b7dd887f8a20394fb7bcaffbd2f2a432f513
matt-sanpedro <m.sanpedro@tcs.com>
2023-10-04 15:19:26 -0700

    Refactored solution with elegant algorithm

--- c2-python-interact-os/week-2/quiz-problems/quiz-02-files-dir/q-05-parent-dir.py
+++ c2-python-interact-os/week-2/quiz-problems/quiz-02-files-dir/q-05-parent-dir.py
@@ -7,13 +7,7 @@ remember that ".." is a relative path alias that means "go up to the parent dire
 def parent_directory():
   # Create a relative path to the parent 
   # of the current working directory 
-    # print(os.getcwd())
-    # print(os.chdir("../"))
+    os.chdir("../")
     return (os.path.abspath(os.getcwd()))
-
-#   relative_parent = os.path.join(os.path.getcwd(), )
-
-  # Return the absolute path of the parent directory
-#   return ___
 
 print(parent_directory())
